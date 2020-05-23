4c67700c4559a59fe9cbbfa37a210f72ef0ae086
ellie <ellie.tms1371@gmail.com>
2020-05-23 15:37:08 +0100

    add functions

--- code.js
+++ code.js
@@ -1,16 +1,9 @@
-let colors=[
-"rgb(255, 0, 0)",
-"rgb(255, 255, 0)",
-"rgb(0, 255, 0)",
-"rgb(0, 255, 255)",
-"rgb(0, 0, 255)",
-"rgb(255, 0, 255)"
-]
-
+let colors= generateRandomColors();
 let squares = document.querySelectorAll(".square");
 console.log(squares)
-let pickColor = colors[3];
+let pickColor = pickRandomColor();
 let colorDisplay = document.querySelector("#colorDisplay");
+let messageDisplay = document.querySelector("#message");
 colorDisplay.textContent = pickColor;
 for(let i=0;i < squares.length ; i++){
 // add initial colors to squares
@@ -18,12 +11,31 @@ for(let i=0;i < squares.length ; i++){
 //add click listeners to squares
 squares[i].addEventListener("click",function(){
 // grab color of clicked square
-let colorClickedSquare =this.style.background
+let colorClickedSquare =this.style.backgroundColor;
 // compare color to pickedColor
-if(colorClickedSquare == pickColor){
-    
+if(colorClickedSquare === pickColor){
+    messageDisplay.textContent = "Correct"
+    changeColors(colorClickedSquare);
+}
+    else{
+        this.style.backgroundColor = "#232323"
+        messageDisplay.textContent = "Try Again"
+    }
+}
+)
 }
 
-})  
+function changeColors(color){
+    // loop through all squares
+    for(let i=0; i < colors.length; i++){
+    // change each color to match given color
+    squares[i].style.backgroundColor = color;
+}
+}
+
+
+function pickRandomColor(){
+let random = Math.floor(Math.random()* colors.length)
+return colors[random];
 
 }
